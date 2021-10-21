6b80ae730ada0327fee5d562adebb98c2ae7a2a3
PAULA REGINA CARDOSO BRUNO <79319807+paulinhabruno@users.noreply.github.com>
2021-10-21 16:57:12 -0300

    Atualização de desafios

---
+++ TipoDeCombustivel.js
@@ -0,0 +1,36 @@
+//continue a solução
+
+let alcohol = 0;
+let gasoline = 0;
+let diesel = 0;
+let code = 0;
+
+while ( code != 4 ) {
+  code = Number(gets());
+  
+    switch (code) {
+    case 1:
+      alcohol++;
+      break;
+      
+    case 2:
+    gasoline++;
+      break;
+      
+    case 3:
+    diesel++;
+      break;
+      
+      case 4:
+      break;
+      
+      default:
+      break;
+      
+  }
+}
+
+console.log("MUITO OBRIGADO");
+console.log("Alcool: " + alcohol);
+console.log("Gasolina: " + gasoline);
+console.log("Diesel: " + diesel);
