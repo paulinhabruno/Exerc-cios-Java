981ab822ae524d678ed092b12badf7bf8ab95714
PAULA REGINA CARDOSO BRUNO <79319807+paulinhabruno@users.noreply.github.com>
2021-10-21 19:14:59 -0300

    Atualizando os desafios.

---
+++ Saida6.js
@@ -0,0 +1,10 @@
+// Em fase de desenvolvimento
+
+print("AMO FAZER EXERCICIO NO URI">);
+print('<AMO FAZER EXERCICIO >');
+print('<AMO FAZER EXERCICIO NO URI>');
+print('<AMO FAZER EXERCICIO NO URI    >');
+print('<AMO FAZER EXERCICIO NO URI>');
+print('<AMO FAZER EXERCICIO >');
+print('<FAZER EXERCICIO>');
+//continue o código aqui
